988828ff18f6183cf11be47b58c49efc0e0872c3
Fernando de Row <ferdr89dev@gmail.com>
2024-08-14 14:17:40 -0300

    remove html tag and update styles

---
+++ src/components/molecules/menu/Navbar.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import css from "./navbar.css";
+import { navbarLinks } from "constants/constans";
+
+function Navbar() {
+  return (
+    <nav className={css.nav}>
+      <ul className={css.list}>
+        <li className={css.item}>
+          <a href="#experience" className={css.link}>
+            {navbarLinks.experience}
+          </a>
+        </li>
+        <li className={css.item}>
+          <a href="#projects" className={css.link}>
+            {navbarLinks.projects}
+          </a>
+        </li>
+        <li className={css.item}>
+          <a href="#about-me" className={css.link}>
+            {navbarLinks.aboutMe}
+          </a>
+        </li>
+        {/* <li className={css.item}>
+          <a href="mailTo:ferderow@gmail.com" className={css.link}>
+            {navbarLinks.contact}
+          </a>
+        </li> */}
+      </ul>
+    </nav>
+  );
+}
+
+export { Navbar };
