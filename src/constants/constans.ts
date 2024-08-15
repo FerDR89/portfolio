3b642d455d1e97827a2b564cb3d97ef05a90376a
Fernando de Row <ferdr89dev@gmail.com>
2024-08-15 09:17:54 -0300

    add constants file

---
+++ src/constants/constans.ts
@@ -0,0 +1,15 @@
+export const greeting = "¡Hola! Soy";
+export const name = "Fernando de Row";
+export const profession = "Frontend Developer";
+
+export const navbarLinks = {
+  experience: "Experiencia",
+  projects: "Proyectos",
+  aboutMe: "Acerca de mí",
+  contact: "Contacto",
+};
+
+export const download = "Descargar cv";
+export const contact = "Contacto:";
+export const aboutMe =
+  "Soy un desarrollador frontend dedicado y comprometido con el éxito de mi equipo y organización. Mi enfoque orientado a objetivos y mi pasión por el aprendizaje continuo me impulsan a enfrentar nuevos desafíos con entusiasmo. \nMe motiva superarme constantemente y crecer profesionalmente, buscando siempre la excelencia en cada proyecto en el que participo. \n\nCuento con experiencia en el desarrollo de aplicaciones utilizando React y React Native, manejo TypeScript de manera efectiva para garantizar un código robusto y mantenible. Mi compromiso con la mejora continua y mi capacidad para adaptarme a las nuevas tecnologías me permiten contribuir significativamente al desarrollo de soluciones innovadoras y de alta calidad.";
