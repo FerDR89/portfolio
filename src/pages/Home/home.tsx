baffb9ff513d18832eb460a5ffbd246631152ac8
Fernando de Row <ferdr89dev@gmail.com>
2024-08-15 09:22:18 -0300

    update home page

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { Layout } from "components/layout/Layout";
-
-import css from "./home.css";
+import Layout from "components/templates/layout/Layout";
+import Welcome from "components/organisms/welcome/Welcome";
+import Experience from "components/organisms/experience/Experience";
+import AboutMe from "components/organisms/about-me/AboutMe";
 
 function Home() {
   return (
-    <section className={css.home}>
-      <Layout />
-    </section>
+    <Layout>
+      <Welcome />
+      <Experience />
+      <AboutMe />
+    </Layout>
   );
 }
 export { Home };
