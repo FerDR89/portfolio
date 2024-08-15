eee2fad6495d3365897ddc80c7b28a78c1d98fff
Fernando de Row <ferdr89dev@gmail.com>
2024-08-15 09:13:23 -0300

    update name vars and extract base url in API

--- src/lib/API/API.ts
+++ src/lib/API/API.ts
@@ -1,70 +1,73 @@
+const BASE_URL =
+  "https://cdn.contentful.com/spaces/nfckvg6bxiqt/environments/master/entries?access_token=UlAA-NdKS06UgWd3isjDU58pLoyrlL1xDJJ6ZcnMRXE&content_type=";
+
 async function fetchProjectsData() {
-  const resultFetch = await fetch(
-    "https://cdn.contentful.com/spaces/nfckvg6bxiqt/environments/master/entries?access_token=UlAA-NdKS06UgWd3isjDU58pLoyrlL1xDJJ6ZcnMRXE&content_type=portfolioProjects"
-  );
+  const resultFetch = await fetch(`${BASE_URL}portfolioProjects`);
 
   const json = await resultFetch.json();
 
-  const arrayData = json.items.map((item) => {
+  const data = json.items.map(({ fields }) => {
     return {
-      imageId: item.fields.image.sys.id,
-      linkDeploy: item.fields.linkDeploy,
-      linkRepo: item.fields.linkRepo,
-      title: item.fields.title,
-      id: item.fields.idProject,
+      imageId: fields.image.sys.id,
+      linkDeploy: fields.linkDeploy,
+      linkRepo: fields.linkRepo,
+      title: fields.title,
+      id: fields.idProject,
     };
   });
 
   const arrayIncludes = json.includes.Asset;
 
-  arrayData.forEach((x) => {
+  data.forEach((x) => {
     let idEncontrado = searchAsset(x.imageId, arrayIncludes);
     x.image = idEncontrado.fields.file.url;
   });
 
-  const arrayDataSorted = arrayData.sort((a, b) => {
+  const sortedData = data.sort((a, b) => {
     return a.id < b.id ? -1 : a.id > b.id ? 1 : 0;
   });
 
-  return arrayDataSorted;
+  return sortedData;
 }
 
 async function fetchTechnologiesIcons() {
-  const resultFetch = await fetch(
-    "https://cdn.contentful.com/spaces/nfckvg6bxiqt/environments/master/entries?access_token=UlAA-NdKS06UgWd3isjDU58pLoyrlL1xDJJ6ZcnMRXE&content_type=technologiesIcons"
-  );
+  const resultFetch = await fetch(`${BASE_URL}technologiesIcons`);
 
   const json = await resultFetch.json();
 
-  console.log(json);
-
-  const arrayData = json.items.map((item) => {
+  const data = json.items.map(({ fields }) => {
     return {
-      imageId: item.fields.icon.sys.id,
-      title: item.fields.title,
-      id: item.fields.iconID,
+      imageId: fields.icon.sys.id,
+      title: fields.title,
+      id: fields.iconID,
     };
   });
 
-  const arrayIncludes = json.includes.Asset;
+  const dataIncludes = json.includes.Asset;
 
-  arrayData.forEach((x) => {
-    let idEncontrado = searchAsset(x.imageId, arrayIncludes);
-    x.image = idEncontrado.fields.file.url;
+  data.forEach((x) => {
+    let idFounded = searchAsset(x.imageId, dataIncludes);
+    x.image = idFounded.fields.file.url;
   });
 
-  const arrayDataSorted = arrayData.sort((a, b) => {
+  const dataSorted = data.sort((a, b) => {
     return a.id < b.id ? -1 : a.id > b.id ? 1 : 0;
   });
 
-  return arrayDataSorted;
+  return dataSorted;
 }
 
 function searchAsset(assetID, arrayIncludes) {
-  const coincidenciaID = arrayIncludes.find((x) => {
+  const matchedID = arrayIncludes.find((x) => {
     return x.sys.id == assetID;
   });
-  return coincidenciaID;
+  return matchedID;
 }
 
-export { fetchProjectsData, fetchTechnologiesIcons };
+async function fetchExperience() {
+  const resultFetch = await fetch(`${BASE_URL}experience`);
+  const json = await resultFetch.json();
+  return json.items[0].fields.data;
+}
+
+export { fetchProjectsData, fetchTechnologiesIcons, fetchExperience };
