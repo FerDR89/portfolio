01a6a5f9e4cd57f9c8200dbc84e679a38c95d247
Fernando de Row <ferdr89dev@gmail.com>
2024-08-17 14:22:31 -0300

    add Stack organisms in home

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -3,12 +3,14 @@ import Layout from "components/templates/layout/Layout";
 import Welcome from "components/organisms/welcome/Welcome";
 import Experience from "components/organisms/experience/Experience";
 import AboutMe from "components/organisms/about-me/AboutMe";
+import Stack from "components/organisms/stack/Stack";
 
 function Home() {
   return (
     <Layout>
       <Welcome />
       <Experience />
+      <Stack />
       <AboutMe />
     </Layout>
   );
