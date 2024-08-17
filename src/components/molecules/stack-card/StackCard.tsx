d74c41ac776a9397349143780d6dfda6a97ba394
Fernando de Row <ferdr89dev@gmail.com>
2024-08-17 14:27:11 -0300

    add stackCard molecule

--- src/components/molecules/stack-card/StackCard.tsx
+++ src/components/molecules/stack-card/StackCard.tsx
@@ -1,23 +1,46 @@
-import React from "react";
+import React, { useRef } from "react";
 import css from "./stackCard.css";
+import Text from "components/atoms/text/Text";
+import { stackHoverBackground } from "constants/constants";
 
 type PropsSearchResult = {
   title: string;
   img: any;
 };
 
-function StackCard(props: PropsSearchResult) {
-  const { title, img } = props;
+const StackCard = ({ title, img }: PropsSearchResult) => {
+  const hoverRef = useRef(null);
+  const mouseOver = (event) => {
+    event.preventDefault();
+    hoverRef.current.style[
+      "box-shadow"
+    ] = `3px 3px 10px ${stackHoverBackground[title]}`;
+  };
+
+  const mouseOut = (event) => {
+    event.preventDefault();
+    hoverRef.current.style["box-shadow"] = "1px 1px 10px rgb(40, 59, 115) ";
+  };
+
   return (
-    <article className={css.container}>
-      <div className={css.img__container}>
+    <div
+      className={css.container}
+      onMouseOver={mouseOver}
+      onMouseOut={mouseOut}
+      ref={hoverRef}
+    >
+      <div className={css.img_container}>
         <img src={img} alt={"Icono de:" + title} className={css.img} />
       </div>
-      <div className={css.title__container}>
-        <p className={css.title}>{title}</p>
+      <div className={css.title_container}>
+        <Text
+          tag="text-bold"
+          text={title}
+          customStyle={{ fontSize: "0.85rem" }}
+        />
       </div>
-    </article>
+    </div>
   );
-}
+};
 
-export { StackCard };
+export default StackCard;
