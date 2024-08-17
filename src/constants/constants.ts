db8fae5683d35db5050de95a6759c70a99b72a43
Fernando de Row <ferdr89dev@gmail.com>
2024-08-17 12:34:35 -0300

    add titles, stackHoverBackground and update navbarLinks in constants

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -7,9 +7,32 @@ export const navbarLinks = {
   projects: "Proyectos",
   aboutMe: "Acerca de mí",
   contact: "Contacto",
+  stack: "Stack",
+};
+
+export const titles = {
+  aboutMe: "Acerca de mi",
+  projects: "Mis proyectos",
+  experience: "Experiencia laboral",
+  stack: "Técnologías y herramientas",
 };
 
 export const download = "Descargar cv";
 export const contact = "Contacto:";
 export const aboutMe =
   "Soy un desarrollador frontend dedicado y comprometido con el éxito de mi equipo y organización. Mi enfoque orientado a objetivos y mi pasión por el aprendizaje continuo me impulsan a enfrentar nuevos desafíos con entusiasmo. \nMe motiva superarme constantemente y crecer profesionalmente, buscando siempre la excelencia en cada proyecto en el que participo. \n\nCuento con experiencia en el desarrollo de aplicaciones utilizando React y React Native, manejo TypeScript de manera efectiva para garantizar un código robusto y mantenible. Mi compromiso con la mejora continua y mi capacidad para adaptarme a las nuevas tecnologías me permiten contribuir significativamente al desarrollo de soluciones innovadoras y de alta calidad.";
+
+export const stackHoverBackground = {
+  TypeScript: "rgb(0,122,204)",
+  ["React JS"]: "rgb(31, 128, 145)",
+  ["React Native"]: "rgb(31, 128, 145)",
+  JavaScript: "rgb(224, 220, 28)",
+  ["Node JS"]: "rgb(140, 200, 75)",
+  ["Express JS"]: "rgb(130, 130, 130)",
+  CSS: "rgb(53, 168, 219)",
+  HTML: "rgb(243, 100, 44)",
+  Firebase: "rgb(245, 130, 12)",
+  PostgreSQL: "rgb(52, 100, 148)",
+  Git: "rgb(244, 84, 52)",
+  Postman: "rgb(255, 108, 55)",
+};
