ab608cb25b70a6a939b224a73599f0f7ee9f23e4
Fernando de Row <ferdr89dev@gmail.com>
2024-08-19 15:45:13 -0300

    add programming image in AboutMe

--- src/components/organisms/about-me/AboutMe.tsx
+++ src/components/organisms/about-me/AboutMe.tsx
@@ -3,16 +3,29 @@ import css from "./about.css";
 import Text from "components/atoms/text/Text";
 import Title from "components/molecules/title/Title";
 import { aboutMe } from "constants/constants";
+import programming from "assets/programming.png";
 
 const AboutMe = () => {
   return (
     <section id="aboutMe" className={css.root}>
       <Title section="aboutMe" />
-      <Text
-        tag="text-body"
-        text={aboutMe}
-        customStyle={{ lineHeight: "1.4", whiteSpace: "pre-line" }}
-      />
+      <div className={css.container}>
+        <div className={css.text_container}>
+          <Text
+            tag="text-body"
+            text={aboutMe}
+            customStyle={{ lineHeight: "1.4", whiteSpace: "pre-line" }}
+          />
+        </div>
+
+        <div className={css.image_container}>
+          <img
+            src={programming}
+            alt={"person programming"}
+            className={css.image}
+          />
+        </div>
+      </div>
     </section>
   );
 };
