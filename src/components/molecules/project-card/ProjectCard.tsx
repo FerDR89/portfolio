b8a169757aa45eb076cd2026b0741b22854b4a33
Fernando de Row <ferdr89dev@gmail.com>
2024-08-22 09:20:00 -0300

    update ProjectCard molecule

--- src/components/molecules/project-card/ProjectCard.tsx
+++ src/components/molecules/project-card/ProjectCard.tsx
@@ -1,56 +1,77 @@
-import React from "react";
+import React, { useRef } from "react";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import Icon from "components/atoms/icon/Icon";
 import Text from "components/atoms/text/Text";
 import MyButton from "components/atoms/my-button/MyButton";
+import { useOberver } from "hooks/hooks";
+import { repository } from "constants/constants";
 import css from "./projectCard.css";
 
 type PropsSearchResult = {
   title: string;
-  img: any;
-  linkDeploy: string;
+  img: string;
   linkRepo: string;
+  description: string;
 };
 
-function ProjectCard(props: PropsSearchResult) {
-  const { title, img, linkDeploy, linkRepo } = props;
+const ProjectCard = ({
+  title,
+  img,
+  linkRepo,
+  description,
+}: PropsSearchResult) => {
+  const containerEl = useRef<HTMLDivElement>(null);
+  const inView: Boolean = useOberver(containerEl);
+
   return (
-    <article className={css.container}>
-      <div className={css["title-container"]}>
+    <article className={`${inView ? css.root : {}}`} ref={containerEl}>
+      <div className={css.title_container}>
         <Text tag="text-bold" text={title} />
       </div>
-      <div className={css["img-container"]}>
-        <a
-          href={linkDeploy}
-          target="_blank"
-          aria-label="Link a demo"
-          className={css.link}
-        >
-          <img
-            src={img}
-            alt={"Vista previa del proyecto:" + title}
-            className={css.project__img}
-          />
-        </a>
-      </div>
-      <div className={css["icons-container"]}>
-        <a
-          href={linkDeploy}
-          target="_blank"
-          aria-label="Link a demo"
-          className={css.link}
-        >
-          <MyButton>Demo</MyButton>
-        </a>
+      <div className={css.img_container}>
         <a
           href={linkRepo}
           target="_blank"
           aria-label="Link a repositorio de Github"
           className={css.link}
         >
-          <MyButton>Repo</MyButton>
+          <img
+            src={img}
+            alt={"Vista previa del proyecto:" + title}
+            className={css.project_img}
+          />
+        </a>
+      </div>
+      <div className={css.text_container}>
+        <Text
+          tag="text-body"
+          text={description}
+          customStyle={{ lineHeight: "1.4" }}
+        />
+      </div>
+      <div className={css.button_container}>
+        <a
+          href={linkRepo}
+          target="_blank"
+          aria-label="Link a repositorio de Github"
+          className={css.link}
+        >
+          <MyButton>
+            <div className={css.icon_container}>
+              <Icon name={faGithub} height="20px" width="20px" />
+              <Text
+                tag="text-body"
+                text={repository}
+                customStyle={{
+                  fontSize: "0.85rem",
+                }}
+              />
+            </div>
+          </MyButton>
         </a>
       </div>
     </article>
   );
-}
+};
 
-export { ProjectCard };
+export default ProjectCard;
