6a03e2501f068594bfc840a739ceee5cb67f023d
Fernando de Row <ferdr89dev@gmail.com>
2025-01-02 11:49:46 -0300

    chore: add aria-label in SocialIcons links

--- src/components/molecules/social-icons/SocialIcons.tsx
+++ src/components/molecules/social-icons/SocialIcons.tsx
@@ -8,17 +8,28 @@ const SocialIcons = () => {
   return (
     <div className={css.social_container}>
       <div className={css.icon_container} id={css.gmail}>
-        <a href="mailto:ferderow@gmail.com">
+        <a
+          href="mailto:ferderow@gmail.com"
+          aria-label="Acceso directo al mail de contacto"
+        >
           <Icon name={faEnvelope} height="40px" width="20px" />
         </a>
       </div>
       <div className={css.icon_container} id={css.linkedin}>
-        <a href="https://www.linkedin.com/in/fernando-de-row/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/fernando-de-row/"
+          target="_blank"
+          aria-label="Acceso directo a perfil de LinkedIn"
+        >
           <Icon name={faLinkedinIn} height="40px" width="20px" />
         </a>
       </div>
       <div className={css.icon_container} id={css.github}>
-        <a href="https://github.com/FerDR89" target="_blank">
+        <a
+          href="https://github.com/FerDR89"
+          target="_blank"
+          aria-label="Acceso directo a perfil de GitHub"
+        >
           <Icon name={faGithub} height="30px" width="30px" />
         </a>
       </div>
